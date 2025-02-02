2825d1c3e3fcd45ec18799e62d0107b8ba57fbd2
Cleve-keem <belloharyourmidey@gmail.com>
2025-02-02 14:18:22 -0800

    reviewing the dialog-content

--- src/components/Budget/AddBudget.jsx
+++ src/components/Budget/AddBudget.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import {
   Dialog,
@@ -13,16 +12,15 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
 function AddBudget() {
-  const [emoji, setEmoji] = useState("😊")
-  const [openEmoji, setOpenEmoji] = useState(false)
+  const [emoji, setEmoji] = useState("😊");
+  const [openEmoji, setOpenEmoji] = useState(false);
   const [budgetName, setBudgetName] = useState();
   const [budgetAmount, setBudgetAmount] = useState();
   const toggleOpenEmoji = () => {
     setOpenEmoji(!openEmoji);
-  }
+  };
   return (
     <div>
-
       <Dialog>
         <DialogTrigger asChild>
           <div className=" bg-gray-100 p-10 rounded-lg items-center flex justify-center border-2  cursor-pointer border-dashed hover:shadow-sm">
@@ -32,28 +30,12 @@ function AddBudget() {
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle > <p className=" text-xl font-bold">Create New Budget</p></DialogTitle>
+            <DialogTitle>
+              {" "}
+              <p className=" text-xl font-bold">Create New Budget</p>
+            </DialogTitle>
 
-            <DialogDescription>
-                </DialogDescription>
-              <div>
-                <Button variant="outline" size="lg" className="text-lg" onClick={toggleOpenEmoji}>{emoji}</Button>
-                <div className=" my-3 absolute bg-white">
-                  <EmojiPicker open={openEmoji} onEmojiClick={(e) => { setEmoji(e.emoji); setOpenEmoji(false) }} />
-                </div>
-                <div>
-                  <p className=" font-semibold text-black my-2">Budget Name</p>
-                  <Input placeholder="e.g Foodstuff" onChange={(e) => setBudgetName(e.target.value)} />
-                </div>
-                <div>
-                  <p className=" font-semibold text-black my-2">Budget Amount</p>
-                  <Input type="number" placeholder="e.g 5000" onChange={(e) => setBudgetAmount(e.target.value)} />
-                </div>
-                <Button
-                  disabled={!(budgetName && budgetAmount)}
-                  className="mt-3 w-full" variant="custom">Create Budget</Button>
-              </div>
-          </DialogHeader>
+            
         </DialogContent>
       </Dialog>
     </div>
