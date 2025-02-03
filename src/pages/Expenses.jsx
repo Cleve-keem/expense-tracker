3ffede3f6cd815d020b2bfc15af4ec71b9a2e398
Cleve-keem <belloharyourmidey@gmail.com>
2025-02-03 11:00:30 -0800

    add expenses section

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -1,11 +1,32 @@
+import ExpensesItem from "@/components/ExpensesItem";
 import React from "react";
-import Sidebar from "../components/Sidebar";
+// import { IoMdTrash } from "react-icons/io";
 
 export default function Expenses() {
   return (
-    <div>
-      <Sidebar />
-      <h1>Expenses</h1>
+    <div className="expenses-wrapper p-4">
+      <h2 className="text-2xl font-extrabold mb-4">My Expenses</h2>
+      <p className="mb-2 font-bold">Latest Expenses</p>
+      <table>
+        <thead>
+          <tr className="">
+            <th>Name</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <ExpensesItem />
+          <ExpensesItem />
+          <ExpensesItem />
+          <ExpensesItem />
+          <ExpensesItem />
+          <ExpensesItem />
+          <ExpensesItem />
+          <ExpensesItem />
+        </tbody>
+      </table>
     </div>
   );
 }
